87efc3e49a397f2913d1e0b1ecedb8e031505835
Felice Forby <forby.1@gmail.com>
2020-10-29 08:20:46 +0900

    Types practice

---
+++ playground/types.js
@@ -0,0 +1,43 @@
+/* eslint-disable */
+const name = 'felice';
+const middle = "april";
+const last = `forby`;
+
+const sentence = "she's so \"cool\"";
+const multiline = `Ohh
+
+ya
+
+This is 
+cool`;
+
+const html = `
+    <p>
+      hey
+    </p>
+`;
+
+const hello = `My name is ${name}. I'm ${1 + 100}`;
+
+const a = 10;
+const b = 20;
+
+const person = {
+    first: 'felice',
+    last: 'forby',
+    age: 100
+};
+
+// Access
+person.first;
+person["first"];
+
+
+let dog;
+console.log(dog); // undefined
+dog = "tipper";
+console.log(dog);
+
+let somethingUndefined;
+const somethingNull = null;
+
